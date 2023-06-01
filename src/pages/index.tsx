a6118cc303d909d39156048b3f207f7552a7944d
Yiwei Diao <tuicaodanad@gmail.com>
2023-06-02 11:50:17 +1200

    remove logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,6 @@ export default function Home() {
             <Breadcrumb />
             <SwitchLayout />
           </nav>
-          <Logo />
           <FileListing />
         </div>
       </main>
