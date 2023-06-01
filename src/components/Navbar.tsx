3e2624ab4f23777e74e3a58b0e2f8ba2571eb2d5
Yiwei Diao <tuicaodanad@gmail.com>
2023-06-02 11:10:21 +1200

    test password dir

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -49,43 +49,41 @@ const Navbar = () => {
     <div className="sticky top-0 z-[100] border-b border-gray-900/10 bg-white bg-opacity-80 backdrop-blur-md dark:border-gray-500/30 dark:bg-gray-900">
       <Toaster />
 
-      <div className="flex items-center justify-between">
-        <div className="mx-auto flex w-full items-center justify-between space-x-4 px-4 py-1">
-          <Link href="/" passHref className="flex items-center space-x-2 py-2 hover:opacity-80 dark:text-white md:p-2">
-            <Image src={siteConfig.icon} alt="icon" width="25" height="25" priority />
-            <span className="block font-bold">{siteConfig.title}</span>
-          </Link>
+      <div className="mx-auto flex w-full items-center justify-between space-x-4 px-4 py-1">
+        <Link href="/" passHref className="flex items-center space-x-2 py-2 hover:opacity-80 dark:text-white md:p-2">
+          <Image src={siteConfig.icon} alt="icon" width="25" height="25" priority />
+          <span className="block font-bold">{siteConfig.title}</span>
+        </Link>
 
-          <div className="flex flex-1 items-center space-x-4 text-gray-700 md:flex-initial">
-            <SwitchLang />
+        <div className="flex flex-1 items-center space-x-4 text-gray-700 md:flex-initial">
+          <SwitchLang />
 
-            {siteConfig.links.length !== 0 &&
-              siteConfig.links.map((l: { name: string; link: string }) => (
-                <a
-                  key={l.name}
-                  href={l.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="flex items-center space-x-2 hover:opacity-80 dark:text-white"
-                >
-                  <FontAwesomeIcon icon={['fab', l.name.toLowerCase() as IconName]} />
-                  <span className="hidden text-sm font-medium md:inline-block">
-                    {
-                      // Append link name comments here to add translations
-                      // t('Weibo')
-                      t(l.name)
-                    }
-                  </span>
-                </a>
-              ))}
-
-            {siteConfig.email && (
-              <a href={siteConfig.email} className="flex items-center space-x-2 hover:opacity-80 dark:text-white">
-                <FontAwesomeIcon icon={['far', 'envelope']} />
-                <span className="hidden text-sm font-medium md:inline-block">{t('Email')}</span>
+          {siteConfig.links.length !== 0 &&
+            siteConfig.links.map((l: { name: string; link: string }) => (
+              <a
+                key={l.name}
+                href={l.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center space-x-2 hover:opacity-80 dark:text-white"
+              >
+                <FontAwesomeIcon icon={['fab', l.name.toLowerCase() as IconName]} />
+                <span className="hidden text-sm font-medium md:inline-block">
+                  {
+                    // Append link name comments here to add translations
+                    // t('Weibo')
+                    t(l.name)
+                  }
+                </span>
               </a>
-            )}
-          </div>
+            ))}
+
+          {siteConfig.email && (
+            <a href={siteConfig.email} className="flex items-center space-x-2 hover:opacity-80 dark:text-white">
+              <FontAwesomeIcon icon={['far', 'envelope']} />
+              <span className="hidden text-sm font-medium md:inline-block">{t('Email')}</span>
+            </a>
+          )}
 
           {tokenPresent && (
             <button
