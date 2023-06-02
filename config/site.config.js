e7a203a4e8bc80a1cc5d3211d1b8d47a2b5aec48
Yiwei Diao <tuicaodanad@gmail.com>
2023-06-02 13:47:37 +1200

    update protected folders names

--- config/site.config.js
+++ config/site.config.js
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/Harmoney', '/安发', 'James Jiang', 'Bruna Rocha', 'Ayame ito'],
+  protectedRoutes: ['/🔐 Harmoney', '/🔐 安发', '🔐 James Jiang', '🔐 Bruna Rocha', '🔐 Ayame ito'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:evon.diao@gmail.com',
