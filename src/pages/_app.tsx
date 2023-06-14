f775a34fd21510b230377c2e4ca6193206232ea9
Yiwei Diao <tuicaodanad@gmail.com>
2023-06-15 09:02:45 +1200

    add vercel Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,9 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 import '../styles/globals.css'
 import '../styles/markdown-github.css'
 
+import { Analytics } from '@vercel/analytics/react';
+
+
 // Require had to be used to prevent SSR failure in Next.js
 // Related discussion: https://github.com/FortAwesome/Font-Awesome/issues/19348
 const { library, config } = require('@fortawesome/fontawesome-svg-core')
@@ -124,6 +127,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <NextNProgress height={1} color="rgb(156, 163, 175, 0.9)" options={{ showSpinner: false }} />
       <Component {...pageProps} />
+      <Analytics />
     </>
   )
 }
