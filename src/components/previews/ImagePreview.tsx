13e4d167254c3ef3c5a3321158e19b9de3e8eb0c
Yiwei Diao <tuicaodanad@gmail.com>
2023-10-02 17:45:07 +1300

    update image size

--- src/components/previews/ImagePreview.tsx
+++ src/components/previews/ImagePreview.tsx
@@ -1,54 +1,33 @@
 import type { OdFileObject } from '../../types'
 
-import { FC, useEffect, useState } from 'react'
+import { FC } from 'react'
+import Image from 'next/image'
 import { useRouter } from 'next/router'
 
 import { PreviewContainer, DownloadBtnContainer } from './Containers'
 import DownloadButtonGroup from '../DownloadBtnGtoup'
 import { getStoredToken } from '../../utils/protectedRouteHandler'
+import { t } from 'i18next'
 
 const ImagePreview: FC<{ file: OdFileObject }> = ({ file }) => {
   const { asPath } = useRouter()
   const hashedToken = getStoredToken(asPath)
 
-  // State variables to store the maximum allowed dimensions
-  const [maxWidth, setMaxWidth] = useState(window.innerWidth)
-  const [maxHeight, setMaxHeight] = useState(window.innerHeight)
+  const isLandscape = file?.image?.width! > file?.image?.height!
 
-  // Update the maximum dimensions when the window is resized
-  useEffect(() => {
-    const handleResize = () => {
-      setMaxWidth(window.innerWidth)
-      setMaxHeight(window.innerHeight)
-    }
-
-    window.addEventListener('resize', handleResize)
-
-    // Clean up the event listener when the component unmounts
-    return () => {
-      window.removeEventListener('resize', handleResize)
-    }
-  }, [])
-
-  // Calculate the actual dimensions of the image
-  const actualWidth = file.image?.width || 0
-  const actualHeight = file.image?.height || 0
-
-  // Calculate the dimensions to fit within the maximum allowed dimensions
-  const scaleFactor = Math.min(maxWidth / actualWidth, maxHeight / actualHeight)
-  const scaledWidth = actualWidth * scaleFactor
-  const scaledHeight = actualHeight * scaleFactor
+  // Determine the dimensions based on the orientation
+  const width = isLandscape ? 800 : undefined // Set width to 800 for landscape images
+  const height = isLandscape ? undefined : 700 // Set height to 700 for portrait images
 
   return (
     <>
       <PreviewContainer>
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img
-          className="mx-auto"
+        <Image
           src={`/api/raw/?path=${asPath}${hashedToken ? `&odpt=${hashedToken}` : ''}`}
           alt={file.name}
-          width={scaledWidth}
-          height={scaledHeight}
+          width={width}
+          height={height}
         />
       </PreviewContainer>
       <DownloadBtnContainer>
