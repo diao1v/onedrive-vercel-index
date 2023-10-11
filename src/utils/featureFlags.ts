cb70ed1d8e0175eceaea803e9607e08927e1e068
Yiwei Diao <tuicaodanad@gmail.com>
2023-10-11 22:08:50 +1300

    fix the gallery view

--- src/utils/featureFlags.ts
+++ src/utils/featureFlags.ts
@@ -1,9 +1,12 @@
+
 export type FeatureFlags = {
   flagGalleryView: boolean
   flagDisableDownload: boolean
+  flagUseCustomSlideInGallery: boolean
 }
 
 export const FEATURE_FLAGS: FeatureFlags = {
   flagGalleryView: false,
   flagDisableDownload: true,
+  flagUseCustomSlideInGallery: true,
 }
