2fa6d397df481d6079db6cbfac2eec371ceb3f4e
Yiwei Diao <tuicaodanad@gmail.com>
2023-10-13 14:13:20 +1300

    using single blur data

--- src/components/gallery/components/AlbumNextJsImage.tsx
+++ src/components/gallery/components/AlbumNextJsImage.tsx
@@ -1,20 +1,25 @@
-import Image from "next/image";
-import type { RenderPhotoProps } from "react-photo-album";
+import Image from 'next/image'
+import type { RenderPhotoProps } from 'react-photo-album'
 
 export default function AlbumNextJsImage({
   photo,
   imageProps: { alt, title, sizes, className, onClick },
   wrapperStyle,
 }: RenderPhotoProps) {
+  const singleColorBlurData =
+    'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mPkE1G8BgABlgEbDKVxAwAAAABJRU5ErkJggg=='
+
   return (
-    <div style={{ ...wrapperStyle, position: "relative" }}>
+    <div style={{ ...wrapperStyle, position: 'relative' }}>
       <Image
         fill
         src={photo}
-        placeholder={"blurDataURL" in photo ? "blur" : undefined}
+        // placeholder={"blurDataURL" in photo ? "blur" : undefined}
+        placeholder="blur"
+        blurDataURL={singleColorBlurData}
         {...{ alt, title, sizes, className, onClick }}
         quality={60}
       />
     </div>
-  );
+  )
 }
