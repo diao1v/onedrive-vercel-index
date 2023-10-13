9453d54769a8f95212c00553807a61172d02afd1
Yiwei Diao <tuicaodanad@gmail.com>
2023-10-13 18:00:52 +1300

    use  unoptimized photo

--- src/components/gallery/components/AlbumNextJsImage.tsx
+++ src/components/gallery/components/AlbumNextJsImage.tsx
@@ -1,28 +1,26 @@
 import Image from 'next/image'
 import type { RenderPhotoProps } from 'react-photo-album'
 
-import {singleColorDataUrl } from '../../../utils/dynamicBlurDataUrl'
+import { singleColorDataUrl } from '../../../utils/dynamicBlurDataUrl'
 
 export default function AlbumNextJsImage({
   photo,
   imageProps: { alt, title, sizes, className, onClick },
   wrapperStyle,
 }: RenderPhotoProps) {
-
   const singleColorBlurData = singleColorDataUrl()
-
 
   return (
     <div style={{ ...wrapperStyle, position: 'relative' }}>
       <Image
         fill
+        unoptimized={true}
         src={photo}
-        // placeholder={"blurDataURL" in photo ? "blur" : undefined}
         placeholder="blur"
         blurDataURL={singleColorBlurData}
         {...{ alt, title, className, onClick }}
         quality={75}
-        sizes='25vh'
+        sizes="25vh"
       />
     </div>
   )
