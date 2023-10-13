5f7ac0b6c1502963c870d11ef5abd797f3c4ae11
Yiwei Diao <tuicaodanad@gmail.com>
2023-10-13 17:51:04 +1300

    remove cache to see

--- next.config.js
+++ next.config.js
@@ -5,8 +5,4 @@ module.exports = {
   reactStrictMode: true,
   // Required by Next i18n with API routes, otherwise API routes 404 when fetching without trailing slash
   trailingSlash: true,
-  images: {
-    minimumCacheTTL: 60,
-    domains: ['source.unsplash.com'],
-  },
 }
